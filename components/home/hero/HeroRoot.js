60bbec145e541a0c63c892ff5d563178311e7e4b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-20 16:17:17 +0600

    Hero section is made responsive for all devices

--- components/home/hero/HeroRoot.js
+++ components/home/hero/HeroRoot.js
@@ -4,8 +4,9 @@ import LeftForm from './LeftForm';
 
 const HeroRoot = () => {
     return (
-        <div className='bg-[url("https://i.ibb.co/BGrLjZm/hero-bg.png")] bg-cover w-full h-[550px]'>
-            <div className="bg-black bg-opacity-40 h-full w-full flex items-center justify-center px-20">
+        <div className='bg-[url("https://i.ibb.co/BGrLjZm/hero-bg.png")] bg-cover w-full md:h-[550px]'>
+
+            <div className="bg-black bg-opacity-60 h-full w-full flex flex-col-reverse md:flex-row items-center justify-center md:px-20">
 
                 <div className="flex-1"><LeftForm /></div>
                 <div className="flex-1"><RightContent /></div>
--- components/home/hero/LeftForm.js
+++ components/home/hero/LeftForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LeftForm = () => {
     return (
         <div>
-            <div className="w-80 h-96 rounded-sm bg-[#1c14005a] bg-opacity-5"></div>
+            <div className="w-96 md:w-80 h-96 bg-[#1c14005a] bg-opacity-5"></div>
         </div>
     );
 };
