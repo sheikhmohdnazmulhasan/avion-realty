352ba149f5c704abe34da2fc5526b13f386d511e
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-20 15:18:54 +0600

    The hero section of the home page has a background image set and an overlay of black over it.

---
+++ components/home/hero/HeroRoot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroRoot = () => {
+    return (
+        <div className='bg-[url("https://i.ibb.co/BGrLjZm/hero-bg.png")] bg-cover w-full h-[550px]'>
+            <div className="bg-black bg-opacity-40 h-full w-full"></div>
+        </div>
+    );
+};
+
+export default HeroRoot;
