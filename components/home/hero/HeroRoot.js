80413515b7c358c5d68df9ddc824c44c09041c88
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-20 16:02:29 +0600

    The structure of Hero's Left From is ready

--- components/home/hero/HeroRoot.js
+++ components/home/hero/HeroRoot.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import RightContent from './RightContent';
+import LeftForm from './LeftForm';
 
 const HeroRoot = () => {
     return (
         <div className='bg-[url("https://i.ibb.co/BGrLjZm/hero-bg.png")] bg-cover w-full h-[550px]'>
-            <div className="bg-black bg-opacity-40 h-full w-full flex items-center">
-                <div className="flex-1"></div>
+            <div className="bg-black bg-opacity-40 h-full w-full flex items-center justify-center px-20">
+
+                <div className="flex-1"><LeftForm /></div>
                 <div className="flex-1"><RightContent /></div>
+
             </div>
         </div>
     );
--- components/home/hero/LeftForm.js
+++ components/home/hero/LeftForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LeftForm = () => {
     return (
         <div>
-            
+            <div className="w-80 h-96 rounded-sm bg-[#1c14005a] bg-opacity-5"></div>
         </div>
     );
 };
