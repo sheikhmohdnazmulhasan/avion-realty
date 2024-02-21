5a25f14821cf751d88a245e66c132746df2db64a
rakibaakter <rakibaakter635@gmail.com>
2024-02-21 17:14:55 +0600

    added contact and route title to footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,6 +1,11 @@
 import logo from "@/public/images/logo.svg";
 import Image from "next/image";
 
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { IoCall } from "react-icons/io5";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className='bg-[url("https://i.ibb.co/JzgqCkk/footer-bg.png)] border-t border-[#9E9E9E] '>
@@ -8,12 +13,82 @@ const Footer = () => {
         <div className="border-b border-[#E4B649] pb-8">
           <Image src={logo} alt="avion realty" />
         </div>
-        <div>
+        <div className="flex justify-between my-8">
           {/* items list div */}
-          <div></div>
+          <div className="text-xl font-light flex gap-12">
+            {/* property type */}
+            <ul>
+              <li>
+                <Link href="/">Apartment</Link>
+              </li>
+              <li>
+                <Link href="/">Penthouse</Link>
+              </li>
+              <li>
+                <Link href="/">Villa</Link>
+              </li>
+              <li>
+                <Link href="/">Townhouse</Link>
+              </li>
+            </ul>
+            {/* popular area */}
+            <ul>
+              <li>
+                <Link href="/">Business bay</Link>
+              </li>
+              <li>
+                <Link href="/">Dubai Marina</Link>
+              </li>
+              <li>
+                <Link href="/">Damac Hills</Link>
+              </li>
+              <li>
+                <Link href="/">Downtown Dubai</Link>
+              </li>
+            </ul>
+            {/* quick links */}
+            <ul>
+              <li>
+                <Link href="/">Off-Plan</Link>
+              </li>
+              <li>
+                <Link href="/">Ready</Link>
+              </li>
+              <li>
+                <Link href="/">Rent</Link>
+              </li>
+              <li>
+                <Link href="/">Sell your property</Link>
+              </li>
+            </ul>
+          </div>
           {/* contact list */}
-          <ul>
-            <li></li>
+          <ul className="space-y-2">
+            <li className="flex gap-2 items-center">
+              <span className="text-[#E4B649] ">
+                <FaMapMarkerAlt size={24} />
+              </span>
+              <a>
+                Office 707, Building No 13
+                <br /> Business Bay, Bay Square, Dubai
+              </a>
+            </li>
+            <li className="flex gap-2 items-center">
+              <span className="text-[#E4B649] ">
+                <MdEmail size={24} />
+              </span>
+              <a>info@avionrealty.ae</a>
+            </li>
+            <li className="flex gap-2 items-center">
+              <span className="text-[#E4B649] ">
+                <IoCall size={24} />
+              </span>
+              <span>
+                <a>+971 50 459 71 67</a>
+                <br />
+                <a>+971 4 876 6085</a>
+              </span>
+            </li>
           </ul>
         </div>
       </div>
