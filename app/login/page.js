5a2d547ceaf5dec720e2c184d56f829a3c2d1d2e
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-22 00:05:35 +0600

    The input field of the login page has been designed

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,4 @@
+"use client"
 import image from '@/public/images/login.jpg';
 import Image from 'next/image';
 import React from 'react';
@@ -5,11 +6,20 @@ import React from 'react';
 const page = () => {
     return (
         <>
-            <div className="flex w-full">
-                <div className="w-[40%]"></div>
-                <div className="w-[60%]">
-                    <Image src={image} alt='login image' />
+            <div className="flex w-full h-screen justify-between">
+                <div className="w-[40%] flex flex-col gap-3 justify-center items-center px-10">
+                    <h2 className='text-3xl mb-16'>Sign in your account </h2>
+                    <form className='flex flex-col w-full'>
+
+                        <label className='mb-2'>Mail Address</label>
+                        <input className='p-3 bg-black border border-[#E4B649] rounded-lg' placeholder='Enter Email Address' type="email" name="" id="" />
+
+                        <label className='mb-2 mt-10'>Password</label>
+                        <input className='p-3 bg-black border border-[#E4B649] rounded-lg' placeholder='Enter Email Address' type="email" name="" id="" />
+                       
+                    </form>
                 </div>
+                <div className="w-[60%]"><Image className='h-screen' src={image} alt='login image' /></div>
             </div>
         </>
     );
