d0b120fe612a358e296bb1042e0e71e622cced0f
rakibaakter <rakibaakter635@gmail.com>
2024-02-22 23:38:31 +0600

    responsive privacy policy fixed

--- app/privacy-policy/page.js
+++ app/privacy-policy/page.js
@@ -1,13 +1,11 @@
 const page = () => {
   return (
-    <div className="px-52 py-12">
+    <div className="px-8 lg:px-52 py-12">
       <h2>hdukg</h2>
-      <div className="space-y-12">
+      <div className="space-y-12 lg:text-xl">
         <div>
-          <h2 className="text-xl font-semibold mb-4">
-            Information You Provide:
-          </h2>
-          <p className="text-xl">
+          <h2 className=" font-semibold mb-4">Information You Provide:</h2>
+          <p>
             Avion Realty Properties LLC collects personal information
             voluntarily provided by you when using our services, including
             contact details (name, email, phone), property preferences, and
@@ -16,10 +14,8 @@ const page = () => {
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
-            Information We Collect:
-          </h2>
-          <p className="text-xl">
+          <h2 className=" font-semibold mb-4">Information We Collect:</h2>
+          <p>
             In addition to provided information, we may collect data related to
             your interactions with our website, such as IP addresses, browser
             details, and device information. This helps us enhance user
@@ -27,10 +23,10 @@ const page = () => {
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
+          <h2 className=" font-semibold mb-4">
             Ways in Which We Use Your Personal Information:
           </h2>
-          <p className="text-xl">
+          <p>
             Avion Realty utilizes your personal information to provide
             personalized real estate services, facilitate property transactions,
             communicate market updates, and improve our services through
@@ -38,20 +34,20 @@ const page = () => {
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
+          <h2 className=" font-semibold mb-4">
             Ways in Which We May Share Your Personal Information:
           </h2>
-          <p className="text-xl">
+          <p>
             We may share your information with trusted partners, service
             providers, or legal authorities as required by law. We do not sell
             or lease your personal information for marketing purposes.
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
+          <h2 className=" font-semibold mb-4">
             Storing Your Personal Information:
           </h2>
-          <p className="text-xl">
+          <p>
             Your data is securely stored using industry-standard measures,
             protecting it from unauthorized access, disclosure, alteration, or
             destruction. We prioritize the privacy and security of your
@@ -59,41 +55,39 @@ const page = () => {
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
-            Requesting Data Deletion:
-          </h2>
-          <p className="text-xl">
+          <h2 className=" font-semibold mb-4">Requesting Data Deletion:</h2>
+          <p>
             You have the right to access, correct, or request the deletion of
             your personal information. Contact us to make such requests.
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">Cookies:</h2>
-          <p className="text-xl">
+          <h2 className=" font-semibold mb-4">Cookies:</h2>
+          <p>
             Our website may use cookies and similar tracking technologies to
             enhance your browsing experience. You can manage your cookie
             preferences through your browser settings.
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">
+          <h2 className=" font-semibold mb-4">
             Links to Third-Party Websites:
           </h2>
-          <p className="text-xl">
+          <p className="">
             Our website may contain links to third-party websites. Avion Realty
             is not responsible for the privacy practices or content of these
             external sites. Please review their privacy policies.
           </p>
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">Contacting Us:</h2>
-          <p className="text-xl">
+          <h2 className=" font-semibold mb-4">Contacting Us:</h2>
+          <p className="">
             For any questions, concerns, or requests regarding your privacy or
             this policy, please contact us.
           </p>
         </div>
         <div>
-          <p className="text-xl">
+          <p>
             By using our services, you agree to the terms outlined in this
             Privacy Policy. Avion Realty Properties LLC is committed to
             protecting your privacy and providing a secure real estate
