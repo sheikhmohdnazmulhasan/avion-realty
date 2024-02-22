23e0d2f623523a16f52392b10a5efa16f35b4629
rakibaakter <rakibaakter635@gmail.com>
2024-02-22 23:30:41 +0600

    created privacy policy route

---
+++ app/privacy-policy/page.js
@@ -0,0 +1,108 @@
+const page = () => {
+  return (
+    <div className="px-52 py-12">
+      <h2>hdukg</h2>
+      <div className="space-y-12">
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Information You Provide:
+          </h2>
+          <p className="text-xl">
+            Avion Realty Properties LLC collects personal information
+            voluntarily provided by you when using our services, including
+            contact details (name, email, phone), property preferences, and
+            transaction information. This information is essential for tailoring
+            our services to your real estate needs.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Information We Collect:
+          </h2>
+          <p className="text-xl">
+            In addition to provided information, we may collect data related to
+            your interactions with our website, such as IP addresses, browser
+            details, and device information. This helps us enhance user
+            experience and improve our services.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Ways in Which We Use Your Personal Information:
+          </h2>
+          <p className="text-xl">
+            Avion Realty utilizes your personal information to provide
+            personalized real estate services, facilitate property transactions,
+            communicate market updates, and improve our services through
+            internal analysis and research.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Ways in Which We May Share Your Personal Information:
+          </h2>
+          <p className="text-xl">
+            We may share your information with trusted partners, service
+            providers, or legal authorities as required by law. We do not sell
+            or lease your personal information for marketing purposes.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Storing Your Personal Information:
+          </h2>
+          <p className="text-xl">
+            Your data is securely stored using industry-standard measures,
+            protecting it from unauthorized access, disclosure, alteration, or
+            destruction. We prioritize the privacy and security of your
+            information.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Requesting Data Deletion:
+          </h2>
+          <p className="text-xl">
+            You have the right to access, correct, or request the deletion of
+            your personal information. Contact us to make such requests.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Cookies:</h2>
+          <p className="text-xl">
+            Our website may use cookies and similar tracking technologies to
+            enhance your browsing experience. You can manage your cookie
+            preferences through your browser settings.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Links to Third-Party Websites:
+          </h2>
+          <p className="text-xl">
+            Our website may contain links to third-party websites. Avion Realty
+            is not responsible for the privacy practices or content of these
+            external sites. Please review their privacy policies.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Contacting Us:</h2>
+          <p className="text-xl">
+            For any questions, concerns, or requests regarding your privacy or
+            this policy, please contact us.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl">
+            By using our services, you agree to the terms outlined in this
+            Privacy Policy. Avion Realty Properties LLC is committed to
+            protecting your privacy and providing a secure real estate
+            experience tailored to your needs.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
