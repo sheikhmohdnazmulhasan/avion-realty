3c086fdafb0ed9835acabac874aa22b27a2a04f2
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-22 15:13:16 +0600

    user's Model Updated

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,9 +3,9 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const { email, role } = await request.json();
     await connectMongoDB();
-    await User.create({ email, role });
+    
+    await User.create(await request.json());
     return NextResponse.json({ message: "The user has been successfully saved to the database" }, { status: 201 })
 
 }
