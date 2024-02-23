5d2de9b50f5fdedf8ee24c14ab48b48da1241fd0
rakibaakter <rakibaakter635@gmail.com>
2024-02-23 23:55:21 +0600

    privacy policy linked up and added new colum to footer and inquiry added to home

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import HeroRoot from "@/components/home/hero/HeroRoot";
+import Inquiry from "@/components/shared/Inquiry";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       {/* <HeroRoot/> */}
 
       <HeroRoot />
+      <Inquiry />
     </main>
   );
 }
