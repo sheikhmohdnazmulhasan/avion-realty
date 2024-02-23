a92de7a94772df3fb9bb6a8ab568b9b1506020e5
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-24 02:00:48 +0600

    handlelogin function implemented with email password and handling with signIn method form next-auth/react

--- app/AuthProvider.js
+++ app/AuthProvider.js
@@ -1,8 +1,14 @@
+'use client'
+
+// Importing the SessionProvider component from the 'next-auth/react' module
 import { SessionProvider } from "next-auth/react";
 
+// Defining a new component called AuthProvider which takes a 'children' prop
 const AuthProvider = ({ children }) => {
 
-    return <SessionProvider>{children}</SessionProvider>
+    // Returning JSX that wraps the 'children' with the SessionProvider
+    return <SessionProvider>{children}</SessionProvider>;
 };
 
+// Exporting the AuthProvider component as the default export
 export default AuthProvider;
