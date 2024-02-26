fe9f25e398c9be1683cb0967789bc920a7be8c02
rakibaakter <rakibaakter635@gmail.com>
2024-02-26 17:59:16 +0600

    fix dashboard height width issue

--- app/(OtherLayout)/dashboard/admin/layout.js
+++ app/(OtherLayout)/dashboard/admin/layout.js
@@ -3,11 +3,11 @@ import "../../../globals.css";
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="flex max-h-screen overflow-hidden">
-      <div className=" shadow-gray-500 shadow-xl">
+    <div className="flex min-h-screen w-full">
+      <div className=" w-1/5 shadow-gray-500 shadow-xl">
         <Sidebar />
       </div>
-      <div>
+      <div className="w-4/5">
         {/* dynamic content */}
         <div>{children}</div>
       </div>
