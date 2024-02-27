76f92d2bdc4460c77f6372b718ceaff92c82fce7
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-27 21:35:35 +0600

    The item's data is faceted

--- app/api/admin/items/area/route.js
+++ app/api/admin/items/area/route.js
@@ -2,6 +2,17 @@ import connectMongoDB from "@/libs/mongodb";
 import AreaItem from "@/models/items/area";
 import { NextResponse } from "next/server";
 
+export async function GET(request) {
+
+    await connectMongoDB();
+    const data = await AreaItem.find();
+    const { searchParams } = new URL(request.url);
+    
+    const params = searchParams.get('nazmul');
+
+    return NextResponse.json(data);
+
+};
 
 // Area POST
 export async function POST(request) {
@@ -10,4 +21,4 @@ export async function POST(request) {
 
     await AreaItem.create(data);
     return NextResponse.json({ message: 'item successfully save to database', success: true }, { status: 200 })
-} 
+};
