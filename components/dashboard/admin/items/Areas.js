5070b9c1d2810ae0156fb79baa840e6970306a4b
rakibaakter <rakibaakter635@gmail.com>
2024-02-27 18:32:57 +0600

    created item box ui

--- components/dashboard/admin/items/Areas.js
+++ components/dashboard/admin/items/Areas.js
@@ -1,7 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa6";
+import { IoMdCloseCircle } from "react-icons/io";
+
 const Areas = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div>
-      <h2>area</h2>
+    <div className="bg-[#161616] p-6 rounded-2xl">
+      <h2 className="text-xl font-semibold">Add/Remove Areas</h2>
+      <div className="my-10 h-[60vh] overflow-y-scroll pr-2">
+        <ul>
+          <li className="flex items-center justify-between">
+            <span>Business Bay</span>
+            <button>
+              <IoMdCloseCircle className="text-red-600 text-xl" />
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center">
+        <button className="bg-[#835C00] rounded-3xl px-3 py-1 flex items-center justify-center">
+          <FaPlus size={16} />
+          <span className="mt-1 ml-1">Add More</span>
+        </button>
+      </div>
     </div>
   );
 };
