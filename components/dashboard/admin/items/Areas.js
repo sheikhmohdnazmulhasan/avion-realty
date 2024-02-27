697d65e6b26f1dbc39f516201817e51e75831113
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-28 02:29:20 +0600

    New area images are hosted on imgbb

--- components/dashboard/admin/items/Areas.js
+++ components/dashboard/admin/items/Areas.js
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { IoMdCloseCircle } from "react-icons/io";
@@ -7,6 +8,26 @@ import { IoMdClose } from "react-icons/io";
 
 const Areas = () => {
   const [openModal, setOpenModal] = useState(false);
+
+  async function handleAddNew(event) {
+    event.preventDefault();
+    const area = event.target.area.value;
+    const areaImage = event.target.areaImage.files[0];
+
+    const image = new FormData()
+    image.append('image', areaImage);
+
+    try {
+      const imgBbResponse = await axios.post(`https://api.imgbb.com/1/upload?key=10a0343a75c20fe85ce07c1d5561bfa1`, image);
+
+      // if (imgBbResponse.data.success)
+
+    } catch (error) {
+
+      throw new Error('Something wrong')
+    }
+
+  }
 
   return (
     <div className="bg-[#161616] p-6 rounded-2xl">
@@ -41,7 +62,7 @@ const Areas = () => {
           </div>
           <div className="bg-[#161616] p-8 rounded-lg shadow shadow-gray-500  text-center">
             <h2 className="mb-6 text-xl font-semibold">Add Areas</h2>
-            <form className=" space-y-6 ">
+            <form className=" space-y-6 " onSubmit={handleAddNew}>
               <input
                 type="text"
                 name="area"
