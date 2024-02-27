8ff7e675c2c91449d034b924fe752d565649bc33
rakibaakter <rakibaakter635@gmail.com>
2024-02-27 19:07:06 +0600

    modal with add area form

--- components/dashboard/admin/items/Areas.js
+++ components/dashboard/admin/items/Areas.js
@@ -21,11 +21,42 @@ const Areas = () => {
         </ul>
       </div>
       <div className="flex justify-center">
-        <button className="bg-[#835C00] rounded-3xl px-3 py-1 flex items-center justify-center">
+        <button
+          onClick={() => setOpenModal(!openModal)}
+          className="bg-[#835C00] rounded-3xl px-3 py-1 flex items-center justify-center"
+        >
           <FaPlus size={16} />
           <span className="mt-1 ml-1">Add More</span>
         </button>
       </div>
+
+      {/* modal for add more items */}
+      {openModal && (
+        <div className="bg-[#161616] p-8 w-1/3 rounded-lg shadow shadow-gray-500 absolute top-1/4 left-1/3 text-center">
+          <h2 className="mb-6 text-xl font-semibold">Add Areas</h2>
+          <form className=" space-y-6 ">
+            <input
+              type="text"
+              name="area"
+              placeholder="write area name"
+              className="bg-black w-full p-2 outline-none border border-dotted rounded-lg"
+            />
+            <br />
+            <input
+              type="file"
+              name="areaImage"
+              placeholder="write area name"
+              className="bg-black w-full p-2  outline-none border border-dotted rounded-lg "
+            />
+            <br />
+            <input
+              type="submit"
+              value="+ Submit"
+              className="bg-[#835C00] px-8 py-1 rounded-2xl"
+            />
+          </form>
+        </div>
+      )}
     </div>
   );
 };
