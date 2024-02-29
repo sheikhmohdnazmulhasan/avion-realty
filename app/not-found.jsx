380877798fbe7388bf0d43b0da8b936720c1f4fb
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-02-29 20:17:15 +0600

    Not found page solution. Which was not working due to absence of layout.jsx in app folder

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import Layout from './(OtherLayout)/layout';
+import "./globals.css";
+
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <h1>Not found page gift for my loving bro Nazmul vai</h1>
+    </Layout>
+  );
+}
