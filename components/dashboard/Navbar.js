c89e0ed5504dedc66124d6104a2d3033450a1430
rakibaakter <rakibaakter635@gmail.com>
2024-02-29 11:59:30 +0600

    handle dropdown btn

--- components/dashboard/Navbar.js
+++ components/dashboard/Navbar.js
@@ -1,15 +1,18 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { GoChevronDown } from "react-icons/go";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
 const Navbar = ({ title, user }) => {
-  console.log(user);
+  const [openModal, setOpenModal] = useState(false);
   const profile = user?.image;
 
   return (
     <div className="px-12 py-10 flex justify-between items-center">
       <h2 className="text-2xl font-semibold">{title}</h2>
-      <div>
+      <div className="flex justify-end items-center">
         <div className="w-12  rounded-full ">
           {profile ? (
             <Image src={profile} alt={user?.name} className="rounded-full" />
@@ -17,6 +20,9 @@ const Navbar = ({ title, user }) => {
             <FaUserCircle size={40} />
           )}
         </div>
+        <button onClick={() => setOpenModal(!openModal)} className="mt-2">
+          {openModal ? <FaAngleUp size={12} /> : <FaAngleDown size={12} />}
+        </button>
       </div>
     </div>
   );
