9a6eef59f3b309e5e026949c4a0c74cbc8341f93
rakibaakter <rakibaakter635@gmail.com>
2024-03-01 18:13:44 +0600

    fetch all agent and viwed in card

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,12 @@
-import Layout from './(OtherLayout)/layout';
+import Layout from "./(OtherLayout)/layout";
 import "./globals.css";
-
 
 export default function NotFound() {
   return (
     <Layout>
-      <h1>Not found page gift for my loving bro Nazmul vai</h1>
+      <div className="bg-black text-white">
+        <h2>hiui</h2>
+      </div>
     </Layout>
   );
 }
