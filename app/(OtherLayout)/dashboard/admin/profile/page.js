6c10d19aab24dfae424a7c63234cf0e986ba6fda
rakibaakter <rakibaakter635@gmail.com>
2024-03-04 14:45:13 +0600

    created useUser hook

--- app/(OtherLayout)/dashboard/admin/profile/page.js
+++ app/(OtherLayout)/dashboard/admin/profile/page.js
@@ -2,26 +2,28 @@
 import EditProfile from "@/components/dashboard/EditProfile";
 import Navbar from "@/components/dashboard/Navbar";
 import UserProfile from "@/components/dashboard/UserProfile";
-import axios from "axios";
-import { useSession } from "next-auth/react";
-import useSWR from "swr";
+import useUser from "@/hooks/useUser";
+// import axios from "axios";
+// import { useSession } from "next-auth/react";
+// import useSWR from "swr";
 
 const Profile = () => {
-  const user = useSession();
+  const { data, error, mutate } = useUser();
+  // const user = useSession();
 
-  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  // const fetcher = (url) => axios.get(url).then((res) => res.data);
 
-  const {
-    data = [],
-    error,
-    mutate,
-  } = useSWR(
-    `http://localhost:3000/api/users?email=${user?.data?.user?.email}`,
-    fetcher
-  );
+  // const {
+  //   data = [],
+  //   error,
+  //   mutate,
+  // } = useSWR(
+  //   `http://localhost:3000/api/users?email=${user?.data?.user?.email}`,
+  //   fetcher
+  // );
 
   return (
-    <div className=" px-12 py-12">
+    <div className=" px-12 py-10">
       <Navbar title="My Profile" />
       <div className="flex mt-12 gap-8 h-full ">
         <div className="w-1/3">
