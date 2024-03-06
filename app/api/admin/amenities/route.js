cc46404212e925064afb07de25f1ebc8b6475e11
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-06 16:46:13 +0600

    Amenities Data successfully saved in database

---
+++ app/api/admin/amenities/route.js
@@ -0,0 +1,18 @@
+import connectMongoDB from "@/libs/mongodb";
+import Amenities from "@/models/amenities";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    await connectMongoDB();
+    const data = await request.json();
+
+    const result = await Amenities.create(data);
+
+    if (!result) {
+        return NextResponse.json({ message: 'Something Wrong', success: false }, { status: 500 });
+
+    } else {
+
+        return NextResponse.json({ message: 'Data successfully saved in database', success: true }, { status: 200 });
+    };
+}
