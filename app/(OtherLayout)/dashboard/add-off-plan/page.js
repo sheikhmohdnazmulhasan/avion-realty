b2f2d8c040dbfd0b56106b1c6cfe4dd874a39bf6
rakibaakter <rakibaakter635@gmail.com>
2024-03-08 19:44:35 +0600

    initiated add off plan route

---
+++ app/(OtherLayout)/dashboard/add-off-plan/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/dashboard/Navbar";
+
+const page = () => {
+  return (
+    <div>
+      <Navbar title="Add Off-Plan Property" />
+    </div>
+  );
+};
+
+export default page;
