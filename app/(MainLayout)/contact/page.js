6c50a4a2f0246351a530cfe90d765ac9aacec381
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-18 00:04:17 +0600

    The banner of the contact page has been designed and ensured to be responsive for all devices

---
+++ app/(MainLayout)/contact/page.js
@@ -0,0 +1,12 @@
+import Banner from '@/components/shared/Banner';
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <Banner title={'Contact US'} />
+        </div>
+    );
+};
+
+export default Contact;
