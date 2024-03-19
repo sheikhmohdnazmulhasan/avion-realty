44db45f27bf42a832f1591342f7fe68789c360f0
rakibaakter <rakibaakter635@gmail.com>
2024-03-19 16:42:58 +0600

    updated the inquiry component

--- app/(MainLayout)/contact/page.js
+++ app/(MainLayout)/contact/page.js
@@ -1,10 +1,12 @@
 import Banner from '@/components/shared/Banner';
+import Inquiry from '@/components/shared/Inquiry';
 import React from 'react';
 
 const Contact = () => {
     return (
         <div className="">
             <Banner title={'Contact US'} />
+            <Inquiry/>
         </div>
     );
 };
