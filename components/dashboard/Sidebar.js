262420a461d3129d5a567e06280117efea25dd03
rakibaakter <rakibaakter635@gmail.com>
2024-03-20 14:51:44 +0600

    added dropdown navlink for podcast in admin based sidebar

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
 import logo from "@/public/images/dashboard/d-logo.svg";
 import Image from "next/image";
 import Link from "next/link";
-import offPlanIcon from "@/public/images/off-plan.svg"
+import offPlanIcon from "@/public/images/off-plan.svg";
 import useUser from "@/hooks/useUser";
 
 import { VscGraph } from "react-icons/vsc";
@@ -21,9 +21,9 @@ import { BsHouseCheck } from "react-icons/bs";
 import { useState } from "react";
 
 const Sidebar = () => {
-  const {data} = useUser();
+  const { data } = useUser();
 
-  const [isDropdown , setDropdown] = useState(false);
+  const [isDropdown, setDropdown] = useState(false);
 
   return (
     <nav className="p-16  min-h-screen">
@@ -38,24 +38,24 @@ const Sidebar = () => {
           </Link>
         </li>
         {/* role based nav links */}
-        {
-          data?.role !== "admin" ?(
-            // for agent nav links
-            <>
+        {data?.role !== "admin" ? (
+          // for agent nav links
+          <>
             <li className="flex gap-4 items-center">
-          
-            <TiThListOutline size={24} />
-            <button
-              onClick={() => setDropdown(!isDropdown)}
-              className=" flex items-center justify-between w-full"
-            >
-              <span>Listings</span>
-              {isDropdown ? <FaAngleUp size={12} /> : <FaAngleDown size={12} />}
-            </button>
-            
-        </li>
-        {isDropdown && (
-              
+              <TiThListOutline size={24} />
+              <button
+                onClick={() => setDropdown(!isDropdown)}
+                className=" flex items-center justify-between w-full"
+              >
+                <span>Listings</span>
+                {isDropdown ? (
+                  <FaAngleUp size={12} />
+                ) : (
+                  <FaAngleDown size={12} />
+                )}
+              </button>
+            </li>
+            {isDropdown && (
               <ul className=" ml-8 space-y-1">
                 <li>
                   <Link href="/dashboard/agent/listing/manage-listing">
@@ -63,70 +63,115 @@ const Sidebar = () => {
                   </Link>
                 </li>
                 <li>
-                <Link href="/dashboard/agent/listing/private-inventory">
+                  <Link href="/dashboard/agent/listing/private-inventory">
                     <span>Private Inventory</span>
                   </Link>
                 </li>
               </ul>
-          )}
-        <li>
-          <Link href="/dashboard/agent/Insights" className="flex gap-4 items-center">
-            <GoGraph size={24} />
-            <span>Insights</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/agent/leads" className="flex gap-4 items-center">
-            <BsCollection size={24} />
-            <span>Leads</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/agent/blog" className="flex gap-4 items-center">
-            <RiFileList3Line size={24} />
-            <span>Blog</span>
-          </Link>
-        </li>
-            </>
-          )
-          :
+            )}
+            <li>
+              <Link
+                href="/dashboard/agent/Insights"
+                className="flex gap-4 items-center"
+              >
+                <GoGraph size={24} />
+                <span>Insights</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/agent/leads"
+                className="flex gap-4 items-center"
+              >
+                <BsCollection size={24} />
+                <span>Leads</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/agent/blog"
+                className="flex gap-4 items-center"
+              >
+                <RiFileList3Line size={24} />
+                <span>Blog</span>
+              </Link>
+            </li>
+          </>
+        ) : (
           // for admin nav links
           <>
-          <li>
-          <Link href="/dashboard/admin/items" className="flex gap-4 items-center">
-            <TiThListOutline size={24} />
-            <span>Items</span>
-          </Link>
-        </li>
-          <li>
-          <Link href="/dashboard/admin/amenities" className="flex gap-4 items-center">
-            <BsHouseCheck size={24} />
-            <span>Amenities</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/agents" className="flex gap-4 items-center">
-            <CgProfile size={24} />
-            <span>Agents</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/listings" className="flex gap-4 items-center">
-            <HiOutlineHomeModern size={24} />
-            <span>Listings</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/upload-podcast" className="flex gap-4 items-center">
-            <GrVideo size={24} />
-            <span>Podcasts</span>
-          </Link>
-        </li>
+            <li>
+              <Link
+                href="/dashboard/admin/items"
+                className="flex gap-4 items-center"
+              >
+                <TiThListOutline size={24} />
+                <span>Items</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/admin/amenities"
+                className="flex gap-4 items-center"
+              >
+                <BsHouseCheck size={24} />
+                <span>Amenities</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/admin/agents"
+                className="flex gap-4 items-center"
+              >
+                <CgProfile size={24} />
+                <span>Agents</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/admin/listings"
+                className="flex gap-4 items-center"
+              >
+                <HiOutlineHomeModern size={24} />
+                <span>Listings</span>
+              </Link>
+            </li>
+            <li className="flex gap-4 items-center">
+              <GrVideo size={24} />
+              <button
+                onClick={() => setDropdown(!isDropdown)}
+                className=" flex items-center justify-between w-full"
+              >
+                <span>Podcasts</span>
+                {isDropdown ? (
+                  <FaAngleUp size={12} />
+                ) : (
+                  <FaAngleDown size={12} />
+                )}
+              </button>
+            </li>
+            {isDropdown && (
+              <ul className=" ml-8 space-y-1">
+                <li>
+                  <Link href="/dashboard/admin/podcast/upload-podcast">
+                    <span>Upload Podcast</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/dashboard/admin/listing/manage-podcast">
+                    <span>Manage Podcast</span>
+                  </Link>
+                </li>
+              </ul>
+            )}
           </>
-        }
+        )}
         <li>
-          <Link href="/dashboard/add-off-plan" className="flex gap-4 items-center">
-            <Image src={offPlanIcon} alt="off-plan"/>
+          <Link
+            href="/dashboard/add-off-plan"
+            className="flex gap-4 items-center"
+          >
+            <Image src={offPlanIcon} alt="off-plan" />
             <span>Add-Off-plan</span>
           </Link>
         </li>
