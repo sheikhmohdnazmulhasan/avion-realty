2c400cda1b1fad8e3fa494069c6abf5e8c3eb37d
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-20 19:08:44 +0600

    Admin's listing page has been modified

--- app/(OtherLayout)/dashboard/admin/listings/page.js
+++ app/(OtherLayout)/dashboard/admin/listings/page.js
@@ -18,22 +18,23 @@ const Listing = () => {
                 <Navbar title={'Manage Listing'} />
                 <div className="mt-20 mb-8 w-full text-sm border border-[#E4B649]">
 
-                    <div className="flex w-full">
-                        <div className="w-[40%] py-3 border border-[#E4B649] text-center">
+                    <div className="flex w-full items-center py-2 border-b border-[#E4B649]">
+                        <div className="w-[40%] border-r border-[#E4B649] py-2 text-center">
                             <p className="font-semibold">Property</p>
                         </div>
-                        <div className="w-[15%] py-3 border border-[#E4B649] text-center">
+                        <div className="w-[15%]  border-r border-[#E4B649] py-2 text-center">
                             <p className="font-semibold">Agent</p>
                         </div>
-                        <div className="w-[15%] py-3 border border-[#E4B649] text-center">
+                        <div className="w-[15%]  border-r border-[#E4B649] py-2 text-center">
                             <p className="font-semibold">Status</p>
                         </div>
-                        <div className="w-[15%] py-3 border border-[#E4B649] text-center">
+                        <div className="w-[15%]  border-r border-[#E4B649] py-2 text-center">
                             <p className="font-semibold">Leads</p>
                         </div>
-                        <div className="w-[15%] py-3 border border-[#E4B649] text-center">
+                        <div className="w-[15%]  py-2 text-center">
                             <p className="font-semibold">Action</p>
                         </div>
+                        <hr />
                     </div>
 
                     {/* each data */}
