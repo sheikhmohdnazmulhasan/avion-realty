a25f181aa7bb7cde2347df8eacf6aef4cbaed35e
rakibaakter <rakibaakter635@gmail.com>
2024-03-20 22:13:31 +0600

    podcast deletaion has been implemented successfully

--- app/(OtherLayout)/dashboard/admin/podcast/manage-podcast/page.js
+++ app/(OtherLayout)/dashboard/admin/podcast/manage-podcast/page.js
@@ -7,10 +7,45 @@ import { IoMdCloseCircle } from "react-icons/io";
 import { RiEditBoxFill } from "react-icons/ri";
 
 import dummyImg from "@/public/images/icon.svg";
+import Swal from "sweetalert2";
+import { mutate } from "swr";
+import axios from "axios";
 
 const UploadPodcast = () => {
   const podcasts = usePodcast();
-  console.log(podcasts);
+  
+  async function handleDeletePodcast(id){
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete podcast!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const serverResponse = await axios.delete(
+            `http://localhost:3000/api/admin/podcast?id=${id}`
+          );
+
+          if (serverResponse.data.success) {
+            Swal.fire({
+              title: "Deleted!",
+              text: `Podcast has been deleted.`,
+              icon: "success",
+            });
+
+            mutate("http://localhost:3000/api/admin/podcast");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  }
+
 
   return (
     <div>
@@ -31,14 +66,14 @@ const UploadPodcast = () => {
               <Image
                 src={dummyImg}
                 alt={podcast.title}
-                className="h-24 w-24 object-fill"
+                className="h-24 w-24 object-fill rounded-md"
               />
               <h2>{podcast.title}</h2>
             </div>
             <h2>{podcast.agent}</h2>
             <h2>{podcast.updatedAt}</h2>
             <div className="flex justify-center items-center gap-4">
-              <button>
+              <button onClick={()=>handleDeletePodcast(podcast._id)}>
                 <IoMdCloseCircle className="text-red-600 text-xl" />
               </button>
               <Link href="/">
