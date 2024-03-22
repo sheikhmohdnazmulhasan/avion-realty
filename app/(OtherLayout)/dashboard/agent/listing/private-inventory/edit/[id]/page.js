cd40d1e19f2e66b6b8e7b75f118a48b9dfb25278
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-23 04:16:58 +0600

    Created PUT indpaint to edit inventory

---
+++ app/(OtherLayout)/dashboard/agent/listing/private-inventory/edit/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditInventory = ({ params }) => {
+
+    return (
+        <div>
+            {params.id}
+        </div>
+    );
+};
+
+export default EditInventory;
