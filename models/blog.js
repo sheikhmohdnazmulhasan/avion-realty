30bb5c96a7be0fb84e88b159656f3fc189ea4f05
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-27 23:10:40 +0600

    blog model updated

--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,13 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const blogSchema = new Schema({ title: String, description: String, img: String });
+const blogSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    blogImg: { type: String, required: true },
+    agent: { type: String, required: true },
+    agentImg: { type: String, required: true },
+
+}, { timestamps: true });
 
 const Blog = mongoose.models.Blogs || mongoose.model('Blogs', blogSchema);
 
