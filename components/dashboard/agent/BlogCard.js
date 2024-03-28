ed3b0089bf602bef2b6541b0ca51ec1ad31cf4be
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-28 17:25:26 +0600

    blog detail functionality has been successfully implemented

--- components/dashboard/agent/BlogCard.js
+++ components/dashboard/agent/BlogCard.js
@@ -4,15 +4,52 @@ import Link from 'next/link';
 import { MdDelete } from 'react-icons/md';
 import edit from '@/public/images/dashboard/listing/edit.svg';
 import live from '@/public/images/dashboard/listing/live.svg';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+import { mutate } from 'swr';
+import useUser from '@/hooks/useUser';
 
 const BlogCard = ({ blog }) => {
-    const { title, agentName, blogImg, updatedAt } = blog;
+    const user = useUser()
+    const { title, agentName, blogImg, updatedAt, _id } = blog;
     const updatedDate = new Date(updatedAt).toLocaleDateString();
+
+    function handleDeleteBlog(_id) {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axios.delete(`http://localhost:3000/api/agent/blog?id=${_id}`).then(res => {
+
+                    if (res.data.success) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: `${title} has been deleted.`,
+                            icon: "success"
+                        });
+
+                        mutate(`http://localhost:3000/api/agent/blog?email=${user?.data?.email}`)
+                    }
+
+                }).catch(err => toast.error('Something Wrong!'))
+
+            }
+        });
+
+    }
 
     return (
         <>
             <div className="flex items-center">
-
+                <Toaster />
                 <div className="w-[45%] p-5">
                     <div className="flex items-center gap-3">
                         <Image width={96} height={96} src={blogImg} alt="Listing image" className="w-16 h-16 rounded-md" />
@@ -33,7 +70,7 @@ const BlogCard = ({ blog }) => {
                 <div className="w-[15%] text-center gap-3 flex items-center justify-center">
                     <Image src={live} alt="Live svg" className="w-4 cursor-pointer hover:scale-125 transition-all" />
                     <Link href={''}>  <Image src={edit} alt="Live svg" className="w-4 cursor-pointer hover:scale-125 transition-all" /></Link>
-                    <MdDelete size={20} className="text-red-500 hover:text-red-600 cursor-pointer hover:scale-125 transition-all" />
+                    <MdDelete size={20} className="text-red-500 hover:text-red-600 cursor-pointer hover:scale-125 transition-all" onClick={() => handleDeleteBlog(_id)} />
                 </div>
 
             </div>
