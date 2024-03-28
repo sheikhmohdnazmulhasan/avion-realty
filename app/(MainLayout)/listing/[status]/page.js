043355f6d8ab1eb45f960f83093b8b74b43df7eb
rakibaakter <rakibaakter635@gmail.com>
2024-03-29 00:58:32 +0600

    changes in responsive description

--- app/(MainLayout)/listing/[status]/page.js
+++ app/(MainLayout)/listing/[status]/page.js
@@ -21,12 +21,14 @@ const ListingDetail = ({ params }) => {
 
     return (
         <div>
-                <div className=' px-4 md:px-12 lg:px-20 py-8 md:py-16 border border-[#262626]'>
+                <div className=' px-4 md:px-12 lg:px-20 py-4 md:py-12 lg:py-16 border-b border-[#262626]'>
                     {
                         params.status === 'Off-Plan' ? (
                             <>
-                                <h2 className='text-3xl'>Discover Luxury Off-Plan Investment Potential in Dubai Real Estate</h2>
-                                <p className='text-[#999] text-xl my-4'>Dubai, nestled between desert and sea, epitomizes modern opulence and enticing investment prospects. Its iconic skyline, adorned with structures like the Burj Khalifa, showcases prime real estate opportunities. Boasting a global business hub, Dubai attracts investors seeking both innovative spaces and thriving communities.
+                                <h2 className='text-3xl lg:hidden'>Dubai Off-Plan Investment Opportunities</h2>
+                                <h2 className='text-2xl hidden lg:block'>Discover Luxury Off-Plan Investment Potential in Dubai Real Estate</h2>
+                                <p className='text-[#999] my-2 lg:hidden'>The city's cultural fusion is mirrored in its real estate offerings, from waterfront residences to golf course estates. Dubai's inclusive atmosphere and diverse communities create an inviting lifestyle for residents and a robust market for investors.</p>
+                                <p className='text-[#999] text-xl my-4 hidden lg:block'>Dubai, nestled between desert and sea, epitomizes modern opulence and enticing investment prospects. Its iconic skyline, adorned with structures like the Burj Khalifa, showcases prime real estate opportunities. Boasting a global business hub, Dubai attracts investors seeking both innovative spaces and thriving communities.
                                 <br/> <br/>
                                 The city's cultural fusion is mirrored in its real estate offerings, from waterfront residences to golf course estates. Dubai's inclusive atmosphere and diverse communities create an inviting lifestyle for residents and a robust market for investors.
                                 <br/> <br/>
@@ -34,18 +36,18 @@ const ListingDetail = ({ params }) => {
                             </>
                         ) : (
                             <>
-                                <h2 className='text-3xl'>Find Your Dream Property</h2>
-                                <p className='text-[#999] text-xl my-4'>Welcome to Estatein, where your dream property awaits in every corner of our beautiful world. Explore our curated selection of properties, each offering a unique story and a chance to redefine your life. With categories to suit every dreamer, your journey </p>
+                                <h2 className='text-2xl lg:text-3xl'>Find Your Dream Property</h2>
+                                <p className='text-[#999] lg:text-xl my-4'>Welcome to Estatein, where your dream property awaits in every corner of our beautiful world. Explore our curated selection of properties, each offering a unique story and a chance to redefine your life. With categories to suit every dreamer, your journey </p>
                             </>
                         )
                     }
                 </div>
-                <div className='px-4 md:px-12 lg:px-20 py-8 md:py-16  space-y-4'>
+                <div className='px-4 md:px-12 lg:px-20 py-8 lg:py-16 space-y-4'>
                     <div className='hidden md:block text-center'>
                         <h2 className='text-[#E4B649] text-3xl font-medium'>Discover a World of Possibilities</h2>
-                        <p className='lg:w-3/5 mx-auto'>Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home</p>
+                        <p className='lg:w-1/2 my-4 mx-auto'>Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home</p>
                     </div>
-                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 py-4'>
                         {
                             data.map(item => <div key={item._id} className='border border-[#CC9107]'>
                                 <div>
