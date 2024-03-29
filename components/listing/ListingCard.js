db69bd37924ddc1f25280e06b13ed23c55c1d2ed
rakibaakter <rakibaakter635@gmail.com>
2024-03-30 03:24:31 +0600

    changes in card ui

--- components/listing/ListingCard.js
+++ components/listing/ListingCard.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import location from '@/public/images/dashboard/listing/location.svg'
 import bathroomSvg from '@/public/images/dashboard/listing/bathroom.svg';
 import sqft from '@/public/images/dashboard/listing/sqft.svg';
 import whatsapp from '@/public/images/contact/whatsapp.svg';
@@ -8,8 +9,9 @@ import bed from '@/public/images/dashboard/listing/bed.svg';
 const ListingCard = ({item}) => {
     return (
        <div className='border border-[#CC9107]'>
-                                <div>
-                                    <Image src={item.images[0]} alt={item.title} width={100} height={300} className='w-full h-52 object-fill'/>
+                                <div className='relative'>
+                                    <Image src={item.images[0]} alt={item.title} width={100} height={300} className='w-full h-52 object-fill '/>
+                                    <div className='bg-black px-4 py-1 rounded-2xl absolute top-4 left-4 text-xs uppercase'><span>{item.status}</span></div>
                                 </div>
                                 <div className='p-4 text-left space-y-2'>
                                     <h2 className='capitalize text-xl font-medium'>{item.title}</h2>
@@ -21,9 +23,11 @@ const ListingCard = ({item}) => {
                                     </div>
 
                                     {/* price */}
-                                    <h2 className='text-xl font-extrabold'><span>AED </span>{item.startingPrice}</h2>
+                                    <h2 className='text-xl font-extrabold'>
+                                        {item.status === 'Off-Plan' && <span className='text-xs text-[#E4B649]  mr-2 font-normal'>Start From</span>}
+                                        <span>AED </span>{item.startingPrice} </h2>
 
-                                    <div className="flex gap-5 pb-4">
+                                    <div className="flex gap-5 pb-4 text-xs">
 
                                         {/* bed */}
                                         <div className="flex items-center gap-2">
@@ -32,14 +36,16 @@ const ListingCard = ({item}) => {
                                         </div>
 
                                         {/* bathroom */}
-                                        <div className="flex items-center gap-2">
-                                            <Image src={bathroomSvg} alt="bathroom svg" />
-                                            <span>{item.bathroom}</span>
-                                        </div>
-
+                                        {item.status === 'Off-Plan' && 
+                                            <div className="flex items-center gap-2">
+                                                <Image src={bathroomSvg} alt="bathroom svg" />
+                                                <span>{item.bathroom}</span>
+                                            </div>
+                                        }
                                         {/* sqft */}
                                         <div className="flex items-center gap-2">
                                             <Image src={sqft} alt="scale svg" />
+                                            {item.status === 'Off-Plan' && <span className='text-xs text-[#E4B649] font-normal'>Start From</span>}
                                             <span>{item.areaSqFt} sq. ft.</span>
                                         </div>
                                     </div>
