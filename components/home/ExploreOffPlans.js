2266a376d642c7a867806be9b1d8c10fe1cdbe62
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-03 20:16:07 +0600

    added some functionality on ExploreOffPlans component

--- components/home/ExploreOffPlans.js
+++ components/home/ExploreOffPlans.js
@@ -16,20 +16,21 @@ const ExploreOffPlans = () => {
     const fetcher = (url) => axios.get(url).then((res) => res.data);
 
     const { data = [], isLoading, error } = useSWR(`http://localhost:3000/api/offplans?status=Off-Plan`, fetcher);
-    
+
     const handlePrev = () => {
-        // if(currentIndex >= 0){
-        //     setCurrentIndex(data.length - 1);
-        // }else {
-        //     setCurrentIndex(currentIndex - 1);
-        // }
 
-        setCurrentIndex(currentIndex - 1);
-        
-    } 
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
 
-    const handleNext = () =>{
-        setCurrentIndex(currentIndex + 1)
+        }
+
+    }
+
+    const handleNext = () => {
+
+        if (currentIndex < data.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        }
     }
 
     return (
@@ -38,8 +39,7 @@ const ExploreOffPlans = () => {
             <p className="text-xl my-4 hidden lg:block">Explore exclusive off-plan investment opportunities in Dubai. Uncover unrivaled luxury, futuristic architecture, and prime locations. Discover a city where innovation meets opulence, creating a lucrative landscape for smart investors. Secure your stake in Dubai&apos;s dynamic real estate market for promising returns and a lifestyle beyond ordinary. Embrace the future of property investment with confidence. </p>
 
             {/* slider  */}
-            {
-                currentIndex && (<div className="my-4 md:rounded-xl flex flex-col-reverse md:flex-row justify-between md:p-4 md:bg-[#171717] border border-[#E4B649] md:border-none ">
+            <div className="my-4 md:rounded-xl flex flex-col-reverse md:flex-row justify-between md:p-4 md:bg-[#171717] border border-[#E4B649] md:border-none ">
 
                 <div className="md:m-4 space-y-4">
                     {/* status */}
@@ -50,13 +50,13 @@ const ExploreOffPlans = () => {
                     <h2 className="text-xl md:text-2xl lg:text-3xl">{data[currentIndex]?.title}</h2>
                     {/* location */}
                     <div className="flex items-center gap-2">
-                        <Image src={location} alt="location svg"  />
+                        <Image src={location} alt="location svg" />
                         <span className=''>{data[currentIndex]?.location}</span>
                     </div>
                     {/* price */}
                     <h2 className='text-xl font-extrabold'>
-                    <span className='text-sm md:text-base text-[#E4B649]  mr-2 font-normal'>Start From</span>
-                    <span>AED </span>{data[currentIndex]?.startingPrice} </h2>
+                        <span className='text-sm md:text-base text-[#E4B649]  mr-2 font-normal'>Start From</span>
+                        <span>AED </span>{data[currentIndex]?.startingPrice} </h2>
 
                     <div className="flex gap-5 pb-4 text-xl">
 
@@ -64,9 +64,9 @@ const ExploreOffPlans = () => {
                         <div className="flex items-center gap-2">
                             <Image src={bed} alt="Bedroom svg" />
                             <span>{data[currentIndex]?.bedroom}</span>
-                        </div>      
+                        </div>
                         {/* sqft */}
-                       <div className="flex items-center gap-2">
+                        <div className="flex items-center gap-2">
                             <Image src={sqft} alt="scale svg" />
                             <span className='text-sm md:text-base text-[#E4B649] font-normal'>Start From</span>
                             <span>{data[currentIndex]?.areaSqFt} sq. ft.</span>
@@ -77,17 +77,17 @@ const ExploreOffPlans = () => {
 
                 {/* image */}
                 <div className="relative md:static">
-                    <Image src={data[currentIndex]?.images[0]} alt={data[currentIndex]?.title} width={280} height={320} className="w-full h-[280px] md:h-full lg:w[280px] lg:h-[320px] object-fill md:rounded-l md:rounded-xl"/>
+                    <Image src={data[currentIndex]?.images[0]} alt={data[currentIndex]?.title} width={280} height={320} className="w-full h-[280px] md:h-full lg:w[280px] lg:h-[320px] object-fill md:rounded-l md:rounded-xl" />
                     <div className='bg-black px-4 py-1 rounded-2xl absolute top-4 left-4 text-xs uppercase md:hidden'><span>{data[currentIndex]?.status}</span></div>
 
                 </div>
-            </div>)
-            }
+            </div>
+
             {/* silde controller */}
             <div className="flex justify-between items-center text-xs px-4">
-                <button onClick={handlePrev}>PREV</button>
-                <button>SHOW ALLL</button>
-                <button onClick={handleNext} >NEXT</button>
+                {<button onClick={handlePrev} className={currentIndex < 1 && 'text-gray-500 cursor-not-allowed'}>PREV</button>}
+                {/* <button>SHOW ALLL</button> */}
+                <button onClick={handleNext} className={currentIndex == data.length - 1 && 'text-gray-500 cursor-not-allowed'}>NEXT</button>
 
             </div>
         </div>
