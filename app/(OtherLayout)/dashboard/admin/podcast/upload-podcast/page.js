bd7f98ecf9481a6c227389c24cde1c7706d8096b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-04 04:52:54 +0600

    Podcast thumbnails have been added

--- app/(OtherLayout)/dashboard/admin/podcast/upload-podcast/page.js
+++ app/(OtherLayout)/dashboard/admin/podcast/upload-podcast/page.js
@@ -3,11 +3,23 @@
 import Navbar from "@/components/dashboard/Navbar";
 import useAgents from "@/hooks/useAgents";
 import axios from "axios";
+import { useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { FaPlus } from "react-icons/fa6";
+import { IoCamera } from "react-icons/io5";
+import { MdOutlineCancel } from "react-icons/md";
 
 const UploadPodcast = () => {
   const agents = useAgents();
+
+  const [showName, setShowName] = useState({});
+  const [showImagePreview, setShowImagePreview] = useState({});
+  const fileInputRef = useRef();
+  const handleClearFile = () => {
+    setShowName('');
+    setShowImagePreview('');
+    fileInputRef.current.value = '';
+  };
 
   function handleAddPodcast(event) {
     event.preventDefault();
@@ -17,6 +29,9 @@ const UploadPodcast = () => {
     const agent = event.target.agent.value;
     const videoUrl = event.target.videoUrl.value;
 
+    const image = new FormData();
+    image.append('image', showName);
+
     const dataForBackend = { title, description, agent, videoUrl };
 
     const urlRegex = new RegExp('^(https?|ftp|file):\\/\\/[\\w\\d\\-\\.%\\?\\=\\+\\&\\/]+', 'i');
@@ -25,7 +40,7 @@ const UploadPodcast = () => {
 
       toast.error('Places Provide Valid Video URL',
         {
-          icon: '👏',
+        
           style: {
             borderRadius: '10px',
             background: '#333',
@@ -38,37 +53,50 @@ const UploadPodcast = () => {
 
     } else {
 
-      axios.post('http://localhost:3000/api/admin/podcast', dataForBackend).then(res => {
+      const toastId = toast.loading('Working...',
+        {
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        }
+      );
+
+      axios.post(`https://api.imgbb.com/1/upload?key=10a0343a75c20fe85ce07c1d5561bfa1`, image).then(res => {
 
         if (res.data.success) {
 
-          toast('Podcast Published',
-            {
-              icon: '👏',
-              style: {
-                borderRadius: '10px',
-                background: '#333',
-                color: '#fff',
-              },
+          const dataForBackend = { title, description, agent, videoUrl, thumbnail: res.data.data.display_url };
+
+          axios.post('http://localhost:3000/api/admin/podcast', dataForBackend).then(res => {
+
+            if (res.data.success) {
+
+              toast.success(`Podcast Published`, { id: toastId });
+
             }
-          );
+
+          }).catch(err => {
+
+            toast.error('Something Wrong!',
+              {
+                style: {
+                  borderRadius: '10px',
+                  background: '#333',
+                  color: '#fff',
+                },
+              }
+            );
+          });
+
         }
 
-      }).catch(err => {
+      }).catch(err => console.log(err))
 
-        toast.error('Something Wrong!',
-          {
-            style: {
-              borderRadius: '10px',
-              background: '#333',
-              color: '#fff',
-            },
-          }
-        );
-      });
+
 
     }
-
 
   }
 
@@ -130,6 +158,39 @@ const UploadPodcast = () => {
             className="bg-black text-xs p-3 rounded-md mt-1 w-full border border-dotted "
           />
         </div>
+
+        <div className="my-10">
+          <label className="">Thumbnail</label>
+          {showName?.name ? (
+            <div className=" mx-auto mt-1.5 flex w-full items-center gap-x-6  rounded-lg border border-dotted p-5 ">
+              <img className="size-[100px] h-[100px] w-full max-w-[150px] rounded-lg object-cover" src={showImagePreview} alt={showName?.name} />
+              <div className="flex-1 space-y-1.5 overflow-hidden">
+                <h5 className=" text-xl font-medium tracking-tight truncate">{showName?.name}</h5>
+                <p className=" text-gray-500">{(showName.size / 1024).toFixed(1)} KB</p>
+              </div>
+              <div onClick={handleClearFile}>
+                <MdOutlineCancel size={24} />
+              </div>
+            </div>
+          ) : (
+            <label className=" mx-auto mt-1.5 flex w-full flex-col items-center justify-center space-y-3 rounded-lg border border-dotted  p-6" htmlFor="file5">
+              <IoCamera size={24} />
+              <div className="space-y-1.5 text-center">
+                <h5 className="whitespace-nowrap text-lg font-medium tracking-tight ">Upload your file</h5>
+                <p className="text-sm text-gray-500">File Should be in PNG, JPEG or JPG formate</p>
+              </div>
+            </label>
+          )}
+
+          <input ref={fileInputRef} onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              const imageFile = e.target.files[0];
+              setShowName(imageFile);
+              setShowImagePreview(URL.createObjectURL(imageFile));
+            }
+          }} className="hidden" id="file5" type="file" />
+        </div>
+
         <div className="flex justify-end mt-6">
           <button className="bg-[#835C00] rounded-3xl px-4 py-2 flex items-center gap-1 justify-center ">
             <FaPlus className="" />
