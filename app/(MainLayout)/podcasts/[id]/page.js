1eb92dd76a84d8ea5eeda3aa03694c44cd714b7b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-05 03:59:15 +0600

    lot of change

--- app/(MainLayout)/podcasts/[id]/page.js
+++ app/(MainLayout)/podcasts/[id]/page.js
@@ -1,6 +1,15 @@
-import React from 'react';
+'use client'
 
+import axios from 'axios';
+import React from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 const PodcastDetails = ({ params }) => {
+
+    const { data = [] } = useSWR(`http://localhost:3000/api/admin/podcast?id=${params.id}`, fetcher);
+
+    console.log(data);
 
     return (
         <div>
--- app/api/admin/podcast/route.js
+++ app/api/admin/podcast/route.js
@@ -6,11 +6,17 @@ export async function GET(request) {
   await connectMongoDB();
 
   const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
   const email = searchParams.get("email");
 
   if (email) {
     const result = await Podcast.findOne({ agent: email });
     return NextResponse.json(result);
+
+  } else if (id) {
+    const result = await Podcast.findById(id);
+    return NextResponse.json(result);
+
   } else {
     const result = await Podcast.find();
     return NextResponse.json(result);
@@ -46,11 +52,11 @@ export async function PUT(request) {
   const result = await Podcast.findByIdAndUpdate(id, data);
 
   if (!result) {
-      return NextResponse.json({ message: 'Something Wrong', success: false }, { status: 500 });
+    return NextResponse.json({ message: 'Something Wrong', success: false }, { status: 500 });
 
   } else {
 
-      return NextResponse.json({ message: 'Data successfully saved in database', success: true }, { status: 200 });
+    return NextResponse.json({ message: 'Data successfully saved in database', success: true }, { status: 200 });
   };
 }
 
