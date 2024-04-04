0af1579e312893f433b1d6e0e349e7f705fd214b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-05 04:59:29 +0600

    The id is extracted from the video URL given by the admin and the iframe of YouTube is shown dynamically

--- app/(MainLayout)/podcasts/[id]/page.js
+++ app/(MainLayout)/podcasts/[id]/page.js
@@ -9,11 +9,13 @@ const PodcastDetails = ({ params }) => {
 
     const { data = [] } = useSWR(`http://localhost:3000/api/admin/podcast?id=${params.id}`, fetcher);
 
-    console.log(data);
 
     return (
         <div>
-            {params.id}
+            {/* video */}
+            <div className="">
+                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${data?.videoUrl?.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/)[1]}`} title="ddddd" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </div>
     );
 };
