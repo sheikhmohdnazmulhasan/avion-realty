caea7afb3b2b4e787c1ad1ac5a39ec3043647687
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-05 06:06:07 +0600

    The podcast details page is fully functional

--- app/(MainLayout)/podcasts/[id]/page.js
+++ app/(MainLayout)/podcasts/[id]/page.js
@@ -1,6 +1,9 @@
 'use client'
 
+import Inquiry from '@/components/shared/Inquiry';
 import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
@@ -8,14 +11,11 @@ const fetcher = (url) => axios.get(url).then((res) => res.data);
 const PodcastDetails = ({ params }) => {
     const [loading, setLoading] = useState(true);
     const [agent, setAgent] = useState([]);
+
     setTimeout(() => { setLoading(false); }, 1000);
 
     const { data = [] } = useSWR(`http://localhost:3000/api/admin/podcast?id=${params.id}`, fetcher);
-
-    useEffect(() => {
-        axios.get(`http://localhost:3000/api/users?email=${data?.agent}`).then(res => setAgent(res.data));
-
-    }, [data])
+    axios.get(`http://localhost:3000/api/users?email=${data?.agent}`).then(res => setAgent(res.data)).catch(err => console.log(err));
 
     if (loading) {
 
@@ -53,14 +53,36 @@ const PodcastDetails = ({ params }) => {
         <div className='px-5 md:px-36 mt-10'>
             {/* video */}
             <div className="">
-                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${data?.videoUrl?.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/)[1]}`} title="ddddd" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe className='w-full h-64 md:h-[350px] lg:h-[550px]' src={`https://www.youtube.com/embed/${data?.videoUrl?.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/)[1]}`} title={data?.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
+
+            <h1 className='text-3xl font-semibold mt-2'>{data?.title}</h1>
 
             {/* updated info */}
             <div className="mt-3 text-[#E4B649]">
                 <p className='font-semibold'>Publish Date: {new Date(data.createdAt).toLocaleDateString()}</p>
 
             </div>
+
+            {/* publisher info */}
+            <div className="flex justify-between mt-6">
+
+                <div className="flex items-center gap-3">
+                    <Image src={agent?.photo} width={40} height={40} alt='agent Profile Picture' className='h-12 w-12 object-cover rounded-full' />
+                    <div className="">
+                        <p>{agent?.name}</p>
+                        <p className='text-sm'>{agent?.designation}</p>
+                    </div>
+                </div>
+                <Link href={`/agents/${agent?._id}`} className='px-2 pt-3 rounded-md text-center border border-[#e4b549a6]'>View Agent</Link>
+            </div>
+
+            {/* Description */}
+            <div className="mt-10">
+                <h1 className='text-xl font-semibold mb-3'>Description</h1>
+                <p>{data?.description}</p>
+            </div>
+            <Inquiry />
         </div>
     );
 };
