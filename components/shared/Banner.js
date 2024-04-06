da74f5c1f92ce0eea19c21028d0eff60a80570a0
rakibaakter <rakibaakter635@gmail.com>
2024-04-07 03:46:32 +0600

    responsive contact route

--- components/shared/Banner.js
+++ components/shared/Banner.js
@@ -6,7 +6,7 @@ const Banner = ({ title }) => {
     return (
         <div className=''>
             <div className="">
-                <Image className='relative' src={img} alt='bannerImg' />
+                <Image src={img} alt='bannerImg' className="h-[200px] md:h-auto relative" />
                 <p className='text-xl md:text-3xl absolute top-32 md:top-64 lg:top-[500px] left-12 md:left-32 lg:left-80 font-semibold'>{title}</p>
                 <div className='h-24 w-24 md:h-36 md:w-36 lg:h-48 lg:w-48 border absolute top-24 md:top-52 lg:top-[420px] left-8 md:left-24 lg:left-60'></div>
             </div>
