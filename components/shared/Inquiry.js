6fb5228b92c71c1bb7b99cf692627b4602480cb4
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-07 22:14:49 +0600

    navbar sticky

--- components/shared/Inquiry.js
+++ components/shared/Inquiry.js
@@ -1,35 +1,38 @@
 import whatsapp from "@/public/images/whatsapp.svg"
 import Image from "next/image";
+import Link from 'next/link';
 
 const Inquiry = () => {
     return (
         <div className='px-4 md:px-12 lg:px-20 py-8 md:py-16 text-center'>
             <div className='uppercase font-light pb-6'>
-            <h2 className='text-xl md:text-2xl lg:text-3xl '>
-                Immediate Assistance With Your Inquiry
-            </h2>
-            <p className='lg:w-3/5 mx-auto my-4 text-base md:text-xl lg:text-2xl '>Explore premier property consultation services in Dubai 
-            for a tailored and insightful real estate experience.</p>
+                <h2 className='text-xl md:text-2xl lg:text-3xl '>
+                    Immediate Assistance With Your Inquiry
+                </h2>
+                <p className='lg:w-3/5 mx-auto my-4 text-base md:text-xl lg:text-2xl '>Explore premier property consultation services in Dubai
+                    for a tailored and insightful real estate experience.</p>
             </div>
 
             {/* form */}
             <form className="flex flex-col lg:flex-row justify-center gap-6">
-                <input type='text' placeholder='Your Name' name="name" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl'/>
-                <input type='number' placeholder='Your Phone' name="phone" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl'/>
-                <input type='email' placeholder='Your Email' name="email" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl'/>
+                <input type='text' placeholder='Your Name' name="name" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl' />
+                <input type='number' placeholder='Your Phone' name="phone" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl' />
+                <input type='email' placeholder='Your Email' name="email" className='bg-transparent border border-[#E4B649] p-3 rounded-2xl' />
 
-                <input type="submit" value="Send" className='bg-[#FFD980] text-xl font-extrabold text-black py-2 px-10 rounded-2xl'/>
+                <input type="submit" value="Send" className='bg-[#FFD980] text-xl font-extrabold text-black py-2 px-10 rounded-2xl' />
             </form>
 
-            <div className='mt-12 flex justify-center gap-1 items-center'>
+            <Link href={'https://wa.me/+971504597167'} target="_blank" className="">
+                <div className='mt-12 flex justify-center gap-1 items-center hover:scale-105 transition-all'>
 
-                <span className="w-8 md:w-12">
-                <Image src={whatsapp} alt="whatsapp" />
-                </span>
+                    <span className="w-8 md:w-12">
+                        <Image src={whatsapp} alt="whatsapp" />
+                    </span>
 
-                <span className=' md:text-2xl'>Get your inquiry on <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#FFD87C] to-[#A27100]'>WhatsApp</span></span>
-            </div>
-            
+                    <span className=' md:text-2xl'>Get your inquiry on <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#FFD87C] to-[#A27100]'>WhatsApp</span></span>
+                </div>
+            </Link>
+
 
         </div>
     );
