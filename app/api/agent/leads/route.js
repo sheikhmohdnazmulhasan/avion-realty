d677b3f08771568952a6beae9a4a6d2545e8426c
mdnazmul42726 <nazmulofficial@outlook.com>
2024-04-17 01:37:13 +0600

    The new query data is successfully saved to the database with full functionality

--- app/api/agent/leads/route.js
+++ app/api/agent/leads/route.js
@@ -1,19 +1,38 @@
 import connectMongoDB from "@/libs/mongodb";
 import Leads from "@/models/leads";
+import OffPlan from "@/models/offPlan";
 import { NextResponse } from "next/server";
 
 export async function POST(required) {
     await connectMongoDB();
     const data = await required.json();
 
-    const result = await Leads.create(data);
+    try {
+        const list = await OffPlan.findById(data.leadFor);
 
-    if (!result) {
-        return NextResponse.json({ message: "Something Wrong", success: false }, { status: 500 });
+        if (!list) {
+            return NextResponse.json({ message: "Something Wrong for finding list by leadFor Id", success: false }, { status: 500 });
 
-    } else {
+        } else {
+            const updateLeadsCount = list.leads + 1;
+            const updateLeadsCountInListing = await OffPlan.findByIdAndUpdate(data.leadFor, { leads: updateLeadsCount });
 
-        return NextResponse.json({ message: "Data successfully saved in database", success: true }, { status: 200 });
+            if (updateLeadsCountInListing) {
+                const result = await Leads.create(data);
+
+                if (!result) {
+                    return NextResponse.json({ message: "Something Wrong for creating new leads", success: false }, { status: 500 });
+
+                } else {
+
+                    return NextResponse.json({ message: "Data successfully saved in database", success: true }, { status: 200 });
+                };
+            }
+
+        };
+
+    } catch (error) {
+        return NextResponse.json({ message: "Something Wrong in main tryc", success: false }, { status: 500 });
     };
 
 };
