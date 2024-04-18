8b105e483c9e454893ff977aeb056aedef27f429
rakibaakter <rakibaakter635@gmail.com>
2024-04-18 23:00:36 +0600

    created filter form in root hero

--- components/home/hero/HeroRoot.js
+++ components/home/hero/HeroRoot.js
@@ -6,7 +6,9 @@ const HeroRoot = () => {
   return (
     <div className='bg-[url("https://i.ibb.co/BGrLjZm/hero-bg.png")] bg-cover w-full md:h-[550px]'>
       <div className="relative bg-black bg-opacity-60 h-full w-full flex flex-col-reverse md:flex-row items-center justify-center md:px-12 lg:px-40">
-        <div className="flex-1">{/* <LeftForm /> */}</div>
+        <div className="flex-1">
+          <LeftForm />
+        </div>
         <div className="flex-1">
           <RightContent />
         </div>
