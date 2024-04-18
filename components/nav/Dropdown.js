8bdb21b854fa56073074c82450203f6f688fc1da
rakibaakter <rakibaakter635@gmail.com>
2024-04-18 15:56:13 +0600

    nav bar follow us click handler changed to mouse movement

--- components/nav/Dropdown.js
+++ components/nav/Dropdown.js
@@ -12,43 +12,64 @@ import {
 } from "react-icons/fa6";
 import { FaAngleDown } from "react-icons/fa6";
 
-const Dropdown = ({ isDrop }) => {
-
-
+const Dropdown = ({ openFollow, setOpenFollow }) => {
   return (
     <span className="mt-1 ">
       <button>
-        {isDrop ? <FaAngleUp size={16} /> : <FaAngleDown size={16} />}
+        {openFollow ? <FaAngleUp size={16} /> : <FaAngleDown size={16} />}
       </button>
 
-      {isDrop && (
-        <ul className=" normal-case text-white bg-[#262626] px-4 py-2 absolute z-10 -left-2 space-y-2">
+      {openFollow && (
+        <ul
+          onMouseLeave={() => setOpenFollow(false)}
+          className=" normal-case mt-4 text-white bg-[#262626] px-4 py-2 absolute z-10 -left-2 space-y-2"
+        >
           <li className="hover:scale-105 transition-all hover:text-[#E8BF44]">
-            <Link href="https://www.facebook.com/avionrealtyuae/" target="_blank" className="flex items-center gap-6 justify-between">
+            <Link
+              href="https://www.facebook.com/avionrealtyuae/"
+              target="_blank"
+              className="flex items-center gap-6 justify-between"
+            >
               <span>Facebook</span>
               <FaFacebookF color="#E8BF44" />
             </Link>
           </li>
           <li className="hover:scale-105 transition-all hover:text-[#E8BF44]">
-            <Link href="https://www.instagram.com/avionrealtyuae/" target="_blank" className="flex items-center justify-between">
+            <Link
+              href="https://www.instagram.com/avionrealtyuae/"
+              target="_blank"
+              className="flex items-center justify-between"
+            >
               <span>Instagram</span>
               <FaInstagram color="#E8BF44" />
             </Link>
           </li>
           <li className="hover:scale-105 transition-all hover:text-[#E8BF44]">
-            <Link href="https://twitter.com/avionrealtyuae" target="_blank" className="flex items-center justify-between">
+            <Link
+              href="https://twitter.com/avionrealtyuae"
+              target="_blank"
+              className="flex items-center justify-between"
+            >
               <span>Twitter</span>
               <FaXTwitter color="#E8BF44" />
             </Link>
           </li>
           <li className="hover:scale-105 transition-all hover:text-[#E8BF44]">
-            <Link href="https://www.linkedin.com/company/avionrealtyuae/" target="_blank" className="flex items-center justify-between">
+            <Link
+              href="https://www.linkedin.com/company/avionrealtyuae/"
+              target="_blank"
+              className="flex items-center justify-between"
+            >
               <span>Linkedin</span>
               <FaLinkedin color="#E8BF44" />
             </Link>
           </li>
           <li className="hover:scale-105 transition-all hover:text-[#E8BF44]">
-            <Link href="https://www.youtube.com/channel/UCipWW3lRblDBIyhzUIby99Q" target="_blank" className="flex items-center justify-between">
+            <Link
+              href="https://www.youtube.com/channel/UCipWW3lRblDBIyhzUIby99Q"
+              target="_blank"
+              className="flex items-center justify-between"
+            >
               <span>Youtube</span>
               <FaYoutube color="#E8BF44" />
             </Link>
