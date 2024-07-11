703438ce3df8a612edb771f38576a7b7c0a6f85c
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-11 22:40:31 +0600

    Forgot password functionality has been implemented

--- app/api/auth/forgot-password/route.js
+++ app/api/auth/forgot-password/route.js
@@ -12,7 +12,7 @@ export async function POST(request) {
         const user = await User.findOne({ email });
 
         if (!user) {
-            return NextResponse.json({ message: 'invalid email' }, { status: 400 })
+            return NextResponse.json({ message: 'invalid email', success: false })
         }
 
         const token = GenerateRandomString(30);
@@ -22,15 +22,15 @@ export async function POST(request) {
         const setToUser = await User.findOneAndUpdate({ email }, dataForForgotPassword);
 
         if (!setToUser) {
-            return NextResponse.json({ message: 'Something Wrong' }, { status: 400 })
+            return NextResponse.json({ message: 'Something Wrong',  success: false })
         }
 
-        return NextResponse.json({ message: 'Email send successfully' }, { status: 200 });
+        return NextResponse.json({ message: 'Email send successfully', token ,  success: true }, { status: 200 });
 
     } catch (error) {
         console.error(error);
 
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+        return NextResponse.json({ message: 'Internal Server Error',  success: false });
     }
 
 }
