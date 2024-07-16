125cb9a96f6aeecee585ad49ed9b2cbe18accd1a
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-16 16:57:21 +0600

    handle more validation for forgot password

--- app/(OtherLayout)/login/page.js
+++ app/(OtherLayout)/login/page.js
@@ -17,9 +17,10 @@ const Login = () => {
     const [openModal, setOpenModal] = useState(false);
     const [email2, setEmail2] = useState(null);
     const [forgotError, setForgotError] = useState('');
+    const [spinner, setSpinner] = useState(false);
 
     // email js configaration
-    const SERVICE_ID = "service_0fnmxk3";
+    const SERVICE_ID = "service_6q2mlgc";
     const TEMPLATE_ID = "template_3q6ydlj";
     const PUBLIC_KEY = "f8-NuZZSnWNj4M3eS";
 
@@ -27,56 +28,66 @@ const Login = () => {
     async function handleForgotPassword(event) {
         event.preventDefault();
         setForgotError('');
+        setSpinner(true);
         const forgotEmail = event.target.forgotEmail.value;
 
         try {
             const response = await axios.post('/api/auth/forgot-password', { email: forgotEmail });
 
             if (response.data.message === 'invalid email') {
+                setSpinner(false);
                 setForgotError('Sorry! Email does not match any accounts');
 
             } else if (response.data.message === 'Something Wrong') {
+                setSpinner(false);
                 setForgotError('Sorry! Something Wrong.');
-                
+
             } else if (response.data.message === 'Internal Server Error') {
+                setSpinner(false);
                 setForgotError('Sorry !Internal Server Error');
 
             } else if (response.data.message === 'Email send successfully') {
 
-                // TODO:  send the verification email;
-                // http://localhost:3000/auth/account/verification/forgot-password/token/${response.data.token}
-                
-
+                // Done:  send the verification email;
                 const templateParams = {
                     forgotEmail,
                     link: `http://localhost:3000/auth/account/verification/forgot-password/token/${response.data.token}`
                 };
-                console.log(templateParams );
 
                 // for testing
                 // setForgotError(response.data.token);
                 //email js
-                emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams , PUBLIC_KEY)
+
+                emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY)
                     .then((result) => {
+                        setSpinner(false);
+                        setOpenModal(false)
                         Swal.fire({
                             icon: 'success',
                             title: 'Verification Email Send!',
                             text: "If you don't find the email in your inbox, please check your spam folder!"
                         });
-                        
                     }, (error) => {
-                        console.log(error.text);
+                        setSpinner(false);
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Something Wrong!',
+                            text: "An unknown error occurred. Please try again later"
+                        });
+
                     });
 
-                
+
 
             } else {
                 setForgotError('Something Wrong.');
+                setSpinner(false);
             }
 
         } catch (error) {
             console.log(error);
             setForgotError('Something Wrong!')
+            setSpinner(false);
         }
     }
 
@@ -125,7 +136,7 @@ const Login = () => {
                                     Email
                                 </label>
                                 <div className="relative">
-                                    <input defaultValue={email2} id="email_navigate_ui_modal" name='forgotEmail' type="email" placeholder="example@gmail.com" className="block w-full rounded-lg p-3 pl-10 outline-none drop-shadow-lg  bg-gray-700 text-white" />
+                                    <input defaultValue={email2} id="email_navigate_ui_modal" name='forgotEmail' type="email" required placeholder="example@gmail.com" className="block w-full rounded-lg p-3 pl-10 outline-none drop-shadow-lg  bg-gray-700 text-white" />
                                     <span className="absolute left-2 top-1/4">
                                         <svg viewBox="0 0 24 24" fill="none" className="inline-block w-6" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fillRule="evenodd" clipRule="evenodd" d="M2 10C2 6.22876 2 4.34315 3.17157 3.17157C4.34315 2 6.22876 2 10 2H14C17.7712 2 19.6569 2 20.8284 3.17157C22 4.34315 22 6.22876 22 10V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V10ZM7.73867 16.4465C8.96118 15.5085 10.4591 15 12 15C13.5409 15 15.0388 15.5085 16.2613 16.4465C17.4838 17.3846 18.3627 18.6998 18.7615 20.1883C18.9044 20.7217 18.5878 21.2701 18.0544 21.413C17.5209 21.556 16.9726 21.2394 16.8296 20.7059C16.5448 19.6427 15.917 18.7033 15.0438 18.0332C14.1706 17.3632 13.1007 17 12 17C10.8993 17 9.82942 17.3632 8.95619 18.0332C8.08297 18.7033 7.45525 19.6427 7.17037 20.7059C7.02743 21.2394 6.47909 21.556 5.94563 21.413C5.41216 21.2701 5.09558 20.7217 5.23852 20.1883C5.63734 18.6998 6.51616 17.3846 7.73867 16.4465ZM10 9C10 7.89543 10.8954 7 12 7C13.1046 7 14 7.89543 14 9C14 10.1046 13.1046 11 12 11C10.8954 11 10 10.1046 10 9ZM12 5C9.79086 5 8 6.79086 8 9C8 11.2091 9.79086 13 12 13C14.2091 13 16 11.2091 16 9C16 6.79086 14.2091 5 12 5Z" className=' fill-white'></path><rect x="2.5" y="2.5" width="19" height="19" rx="3.5" className=' stroke-white'></rect></g></svg>
                                     </span>
@@ -136,7 +147,8 @@ const Login = () => {
                             {/* button type will be submit for handling form submission*/}
                             <div className="  flex justify-end mt-6">
 
-                                <button type="submit" className="  py-2.5 px-5 rounded-lg drop-shadow-lg bg-gray-700 hover:bg-gray-800">
+                                <button type="submit" className=" flex gap-2 justify-center items-center py-2.5 px-3 rounded-lg drop-shadow-lg bg-gray-700 hover:bg-gray-800">
+                                    {spinner && <div className="size-7 animate-[spin_2s_linear_infinite] rounded-full border-4 border-dashed border-[#d2991f]"></div>}
                                     Send Verification Email
                                 </button>
                             </div>
